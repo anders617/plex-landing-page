f2aba167eb253faac9ce2b44fab2f471d1a1209e
Ryan Johnston <ryjo1026@gmail.com>
2019-06-05 23:38:52 -0400

    Initial file setup for server status feature
    Planning on using some simple js to accomplish this. Unsure of the best
    way to connect to the server.

---
+++ assets/js/status.js
@@ -0,0 +1,9 @@
+function checkServerStatus() {
+    // TODO check either the Plex API (preferrable) or ping the server 
+}
+
+function updateServerStatus() {
+    // TODO update proper div with server status
+}
+
+updateServerStatus();
